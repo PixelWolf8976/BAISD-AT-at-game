0c03502e8ebb18ad479c6bb43a18b27054558ab9
PixelWolf8976 <156145502+PixelWolf8976@users.noreply.github.com>
2024-02-12 12:49:28 -0500

    Thingys

--- Resources/Player1.gd
+++ Resources/Player1.gd
@@ -14,15 +14,8 @@ var rotationSpeed = 0.0
 var isOnWall = false
 
 func _physics_process(delta):
-	#if Input.is_action_pressed("drive_forward"):
-	#	if speed < currentMaxSpeed:
-	#		speed += acceleration * delta
-	#else:
-	#	if speed > 0:
-	#		speed -= acceleration * delta
-	#	if speed < 0:
-	#		speed = 0
-	
+	print(delta)
+	print(1/delta)
 	
 	if speed < currentMaxSpeed:
 		speed += acceleration * delta
@@ -31,12 +24,12 @@ func _physics_process(delta):
 	
 	# code for left & right turns
 	
-	if Input.is_action_pressed("turn_left"):
+	if Input.is_action_pressed("turn_left_1"):
 		if rotationSpeed > 0:
 			rotationSpeed -= rotationAcceleration * delta
 		if rotationSpeed > -rotationMaxSpeed:
 			rotationSpeed -= rotationAcceleration * delta
-	elif Input.is_action_pressed("turn_right"):
+	elif Input.is_action_pressed("turn_right_1"):
 		if rotationSpeed < 0:
 			rotationSpeed += rotationAcceleration * delta
 		if rotationSpeed < rotationMaxSpeed:
--- Resources/Player2.gd
+++ Resources/Player2.gd
@@ -1,75 +1,74 @@
-extends Area2D
-
-signal hitWall
+extends CharacterBody2D
 
 @export var defaultMaxSpeed = 500.0
 @export var corneringMaxSpeed = 475.0
+@export var wallMaxSpeed = 400.0
 @export var acceleration = 200.0
 
 @export var rotationMaxSpeed = 5
 @export var rotationAcceleration = 5
 
-var currentMaxSpeed
+var currentMaxSpeed = defaultMaxSpeed
 var speed = 0.0
 var rotationSpeed = 0.0
-var screen_size
+var isOnWall = false
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	screen_size = get_viewport_rect().size
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	var velocity = Vector2.ZERO # player movement vector
+func _physics_process(delta):
 	
-	if Input.is_action_pressed("drive_forward"):
-		if speed < currentMaxSpeed:
-			speed += acceleration * delta
-	else:
-		if speed > 0:
-			speed -= acceleration * delta
-		if speed < 0:
-			speed = 0
-	print (speed)
+	if speed < currentMaxSpeed:
+		speed += acceleration * delta
+	
 	velocity = Vector2(speed, 0).rotated(rotation)
 	
-	if Input.is_action_pressed("turn_left"):
+	# code for left & right turns
+	
+	if Input.is_action_pressed("turn_left_2"):
 		if rotationSpeed > 0:
 			rotationSpeed -= rotationAcceleration * delta
 		if rotationSpeed > -rotationMaxSpeed:
 			rotationSpeed -= rotationAcceleration * delta
-	elif Input.is_action_pressed("turn_right"):
+	elif Input.is_action_pressed("turn_right_2"):
 		if rotationSpeed < 0:
 			rotationSpeed += rotationAcceleration * delta
 		if rotationSpeed < rotationMaxSpeed:
 			rotationSpeed += rotationAcceleration * delta
 	else:
 		if rotationSpeed < 0:
-			currentMaxSpeed = corneringMaxSpeed
+			if isOnWall:
+				currentMaxSpeed = wallMaxSpeed
+			else:
+				currentMaxSpeed = corneringMaxSpeed
 			rotationSpeed += rotationAcceleration * 2 * delta
 			if rotationSpeed > 0:
 				rotationSpeed = 0
+			if isOnWall:
+				currentMaxSpeed = wallMaxSpeed
+			else:
 				currentMaxSpeed = defaultMaxSpeed
 		elif rotationSpeed > 0:
-			currentMaxSpeed = corneringMaxSpeed
+			if isOnWall:
+				currentMaxSpeed = wallMaxSpeed
+			else:
+				currentMaxSpeed = corneringMaxSpeed
 			rotationSpeed -= rotationAcceleration * 2 * delta
 			if rotationSpeed < 0:
 				rotationSpeed = 0
+			if isOnWall:
+				currentMaxSpeed = wallMaxSpeed
+			else:
 				currentMaxSpeed = defaultMaxSpeed
 		else:
-			currentMaxSpeed = defaultMaxSpeed
+			if isOnWall:
+				currentMaxSpeed = wallMaxSpeed
+			else:
+				currentMaxSpeed = defaultMaxSpeed
 	
 	rotation += rotationSpeed * delta
 	
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 	
-	position += velocity * delta
-	pass
-
-
-func _on_body_entered(body):
-	
-	pass # Replace with function body.
+	if move_and_slide():
+		isOnWall = true
+	else:
+		isOnWall = false
