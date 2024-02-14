656d3b16c1b3bfd250eeeeca957abb1723aa25cb
PixelWolf8976 <156145502+PixelWolf8976@users.noreply.github.com>
2024-02-14 13:46:12 -0500

    Added background for track 1

--- Resources/Level1.gd
+++ Resources/Level1.gd
@@ -3,7 +3,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	position.x = 100000
+	#position.x = 100000
 	pass # Replace with function body.
 
 
