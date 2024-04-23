0da3af831af7588066e92711b03acdc9b3831ec3
PixelWolf8976 <156145502+PixelWolf8976@users.noreply.github.com>
2024-04-23 14:30:40 -0400

    tried overpass

--- Resources/splitscreen test.gd
+++ Resources/splitscreen test.gd
@@ -5,5 +5,5 @@ func _ready():
 	var viewport2 = $HBoxContainer/Viewport2/SubViewport2
 	var camera1 = $HBoxContainer/Viewport1/SubViewport1/Camera2D
 	var camera2 = $HBoxContainer/Viewport2/SubViewport2/Camera2D
-	var world = $HBoxContsainer/Viewport1/SubViewport1/base_test_scene
+	var world = $HBoxContainer/Viewport1/SubViewport1/Level
 	viewport2.world_2d = viewport1.world_2d
