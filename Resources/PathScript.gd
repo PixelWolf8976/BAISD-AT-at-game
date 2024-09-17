5448f9ea4f96c65adfab0336f7e527a4940c2c0b
ObviouslyNerd2 <158509567+ObviouslyNerd2@users.noreply.github.com>
2024-09-17 14:26:56 -0400

    added new overpass texture

--- Resources/PathScript.gd
+++ Resources/PathScript.gd
@@ -1,11 +1,17 @@
 extends Node2D
 
 @export var colLay := 1
+@export var isOverpass := false 
 
 @onready var bar1 := $StaticBody2D
 @onready var bar2 := $StaticBody2D2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	bar1.collision_layer = 1 << colLay | 1 << 0
-	bar2.collision_layer = 1 << colLay | 1 << 0
+	bar1.collision_layer = 1 << colLay | 1 << 0 | 1 << 1
+	bar2.collision_layer = 1 << colLay | 1 << 0 | 1 << 1
+	
+	if isOverpass:
+		$StaticBody2D/Sprite2D.visible = false
+		$StaticBody2D2/Sprite2D.visible = false
+		$Sprite2D.texture = preload("res://Resources/Overpass Road.png")
