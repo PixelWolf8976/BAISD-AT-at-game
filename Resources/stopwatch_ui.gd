722363aee2a1d24ecc9a25c1ee44adb050dce8c5
PixelWolf8976 <156145502+PixelWolf8976@users.noreply.github.com>
2024-09-25 13:57:28 -0400

    Added thingys

--- Resources/stopwatch_ui.gd
+++ Resources/stopwatch_ui.gd
@@ -5,18 +5,20 @@ var stopwatchRunning := true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if stopwatchRunning:
-		elapsedTime += delta
-		var out := str(round(elapsedTime * 100.0) / 100.0)
-		var splitStr = out.split(".")
-		if splitStr.size() > 1:
-			for i in range(2 - splitStr[1].length()):
-				out += "0"
-		$Label.text = out
+	if !get_tree().paused:
+		if stopwatchRunning:
+			elapsedTime += delta
+			var out := str(round(elapsedTime * 100.0) / 100.0)
+			var splitStr = out.split(".")
+			if splitStr.size() > 1:
+				for i in range(2 - splitStr[1].length()):
+					out += "0"
+			$Label.text = out
 
 ## For once race is finished, look for input to go back to menu
 func _input(event: InputEvent):
 	if !stopwatchRunning && event is InputEventKey && event.is_pressed():
+		get_tree().paused = false
 		get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
 
 func startStopwatch():
@@ -27,6 +29,7 @@ func stopStopwatch():
 
 func finishRace(winner):
 	if stopwatchRunning:
+		get_tree().paused = true
 		stopStopwatch()
 		$WinnerText.visible = true
 		$PressKeyToExit.visible = true
