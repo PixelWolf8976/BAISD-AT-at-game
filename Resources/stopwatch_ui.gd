5510ec373d504db175f04d5b26cfa2fc2be75aaa
PixelWolf8976 <156145502+PixelWolf8976@users.noreply.github.com>
2024-11-13 12:48:39 -0500

    Added saving scores and whatnot

--- Resources/stopwatch_ui.gd
+++ Resources/stopwatch_ui.gd
@@ -4,6 +4,8 @@ var elapsedTime := 0.0
 var stopwatchRunning := true
 var endElapsedTime := 0.0
 var finishRaceWaitTime := 1.0
+var textFocused := false
+var winnerType := ""
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -23,9 +25,16 @@ func _process(delta: float) -> void:
 
 ## For once race is finished, look for input to go back to menu
 func _input(event: InputEvent):
-	if !stopwatchRunning && event is InputEventKey && event.is_pressed() && endElapsedTime > finishRaceWaitTime:
+	if !textFocused && !stopwatchRunning && event is InputEventKey && event.is_pressed() && endElapsedTime > finishRaceWaitTime:
+		var name: String = $LineEdit.text
+		if name.is_empty():
+			name = winnerType
+		Global.saveScoreToFile(name, elapsedTime)
+		
 		get_tree().paused = false
 		get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
+	elif textFocused && !stopwatchRunning && event is InputEventKey && event.is_pressed() && event.is_action_pressed("Enter"):
+		$LineEdit.release_focus()
 
 func startStopwatch():
 	stopwatchRunning = true
@@ -39,14 +48,20 @@ func finishRace(winner):
 		stopStopwatch()
 		$WinnerText.visible = true
 		$TextureRect.visible = true
+		$LineEdit.visible = true
 		$WinnerText.text = winner.type + " wins!"
+		winnerType = winner.type
 		if winner.type == "Red":
 			$WinnerText.label_settings.font_color = Color(255, 0, 0)
 		else:
 			$WinnerText.label_settings.font_color = Color(0, 0, 255)
-	
-	Global.saveScoreToFile("Bob", elapsedTime)
 
 func resetStopwatch():
 	stopwatchRunning = false
 	elapsedTime = 0.0
+
+func _on_line_edit_focus_entered() -> void:
+	textFocused = true
+
+func _on_line_edit_focus_exited() -> void:
+	textFocused = false
